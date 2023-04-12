1eec16b30db30db6d9b0a0266119fa5ebf3ed369
system5081 <nkrexkai@gmail.com>
2023-04-12 17:19:49 +0900

    Router Comp01

---
+++ src/components/BlogPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div>
+      <p>BlogPage</p>
+    </div>
+  )
+}
+
+export default BlogPage
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,22 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+        <div className='logo'>
+            <a href='http://localhost:3000/'><h3>Mypage</h3></a>
+        </div>
+        <nav>
+            <ul>
+
+                <li>
+                    <a href='http://localhost:3000/blog'>Blog</a>     
+                </li>
+                <li>
+                    <a href='https://github.com/system5081'>GitHub</a>     
+                </li>
+            </ul>
+        </nav>
+    </header>
   );
 };
 
