ddba1059d64ef1f8c1a1e053d94bb3fc28c930e6
system5081 <nkrexkai@gmail.com>
2023-04-16 18:00:40 +0900

    build041601

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,96 +1,102 @@
 import React from 'react'
-import reactImage from "../Images/react.png";
+import pythontImage from "../Images/Python.png";
 import jsImage from "../Images/js.png";
 import firebaseImage from "../Images/firebase.jpg";
-import profileImage from "../Images/sibainu.jpg";
+import profileImage from "../Images/seabass.jpg";
+import movieImage from "../Images/movie3.png";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import StorageIcon from '@mui/icons-material/Storage';
+import DevicesIcon from '@mui/icons-material/Devices';
+
+
 
 const HomePage = () => {
   return (
     <div className="container text-center">
-      <h1>Shin Code</h1>
 
-      <img src={profileImage} className="profileImage" />
+      <div className='test'>
+      <h1>system5081</h1>
 
-      <p>
-        ShinCodeです。普段は個人でWeb系の情報発信をYoutubeやTwitterにて行っています。主にHTML/CSS/Javascript/Reactメインに取り扱っています。趣味は個人でプログラミングの撮影を行うことです。
-        ShinCodeです。普段は個人でWeb系の情報発信をYoutubeやTwitterにて行っています。主にHTML/CSS/Javascript/Reactメインに取り扱っています。趣味は個人でプログラミングの撮影を行うことです。
-      </p>
+      <img src={profileImage} className="profileImage" alt="プロフィール" />
 
-      <section class="page-section" id="services">
+      <p>趣味で個人用アプリケーション開発をしてます。</p>
+        <p>デジタル技術系の備忘録としてのBlogを始めました。<br/>今年から趣味の釣りの記録用のページを始めようと思い準備中です。シーズンまで後少し!!</p>
+      <br/>
+      </div>
+      <section className="page-section" id="services">
         <div className="service">
-          <div class="text-center">
-            <h2 class="section-heading text-uppercase">PORTFOLIO</h2>
-            <h3 class="section-subheading text-muted mb-5">
-              私が作った作品一覧です
-            </h3>
+          <div className="text-center">
+            <h2 className="section-heading text-uppercase">List of Application</h2>
+            <br />
+            <h4 className="section-subheading text-muted mb-5">
+              私が制作（中）のアプリケーションです
+            </h4>
           </div>
-          <div class="row text-center">
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 class="my-3">ECサイト</h4>
-              <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
+          <div className="col-md-4">
             </div>
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 class="my-3">レスポンシブサイト</h4>
-              <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
+          <div className="row text-center" >
+            <div className="col-md-4 ">
+                <div className='center-col test'>
+                <a className="apptitle " href='https://moviereact.system5081.com'>
+                  <br/>
+                  <h4 className="my-3">動画アプリケーション</h4>
+                  <br />
+                  <img src={movieImage} className="img-fluid" alt="動画アプリケーションの画像"/>
+                </a>
+                <p className='center-col2'>
+                  <br/>
+                  　動画を扱うアプリケーション。
+                  認証済みユーザに限定公開するために今後の開発を進めていきたい。<br /><br /><br/>
+                  <DevicesIcon />　React<br/>
+                  <a className="apptitle " href='https://github.com/system5081/movie_react18'>　<GitHubIcon />movie_react18<br /></a>
+                  <StorageIcon />　DjangoRF<br/>
+                  <a className="apptitle " href='https://github.com/system5081/test_api'>　<GitHubIcon />test_api<br /></a><br/>
+                  
+                  </p>
+                </div>
             </div>
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 class="my-3">ウェブセキュリティ</h4>
-              <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
+            <div className="col-md-4 ">
+                <div className='center-col test'>
+                <a className="apptitle " href='#'>
+                  <br/>
+                  <h4 className="my-3">CSV計算機</h4>
+                  <br />
+                  <img src={movieImage} className="img-fluid" alt="動画webアプリケーションの画像"/>
+                </a>
+                <p className='center-col2'>
+                  <br/>
+                  　特定の計算話するために開発。
+                  アプリケーションまで作りましたが、エンジニアが扱うわけではないので保守しやすい方が良さそうと考えています。<br /><br />
+                  <DevicesIcon />　React<br/>
+                  <a className="apptitle " href='https://github.com/system5081/csv_react'>　<GitHubIcon />csv_react<br /></a>
+                  <StorageIcon />　DjangoRF<br/>
+                  <a className="apptitle " href='https://github.com/system5081/csv_api'>　<GitHubIcon />csv_api<br /></a><br/>
+                  </p>
+                </div>
+            </div>
+            <div className="col-md-4 ">
+                <div className='center-col test'>
+                <a className="apptitle " href='#'>
+                  <br/>
+                  <h4 className="my-3">Instaクローン</h4>
+                  <br />
+                  <img src={movieImage} className="img-fluid" alt="動画webアプリケーションの画像"/>
+                </a>
+                <p className='center-col2'>
+                  <br/>
+                  　ユーザ間のやり取りについて学ぶために開発
+                  作ってはみたものの、あまり活用できそうなところはなかったかもしれません。<br /><br />
+                  <DevicesIcon />　React<br/>
+                  <a className="apptitle " href='https://github.com/system5081/csv_react'>　<GitHubIcon />csv_react<br /></a>
+                  <StorageIcon />　DjangoRF<br/>
+                  <a className="apptitle " href='https://github.com/system5081/csv_api'>　<GitHubIcon />csv_api<br /></a><br/>
+                  </p>
+                </div>
+            </div>
             </div>
           </div>
-        </div>
       </section>
-
-      <section id="skill">
-        <div class="text-center">
-          <h1 class="title">スキル</h1>
-          <div class="row text-center">
-            <div class="col-md-4 services">
-              <img src={reactImage} />
-              <h4>React</h4>
-              <p>Reactがつかえます</p>
-            </div>
-            <div class="col-md-4 services">
-              <img src={jsImage} />
-              <h4>HTML/CSS</h4>
-              <p>HTML/CSSがつかえます</p>
-            </div>
-            <div class="col-md-4 services">
-              <img src={firebaseImage} />
-              <h4>Firebase</h4>
-              <p>Firebaseがつかえます</p>
-            </div>
-          </div>
-          <button type="button" class="btn btn-primary">
-            スキル一覧
-          </button>
         </div>
-      </section>
-    </div>
   )
 }
 
